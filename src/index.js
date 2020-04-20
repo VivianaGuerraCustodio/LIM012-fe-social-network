9f01e6fa449c4e6486e295d2aeda195b39a89fe4
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-04-20 12:35:34 -0500

    indexJS vacio

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
 import { example } from './example.js';
 
 example();
-
-
-document.querySelector('.menu_bar').addEventListener('click',()=>{
-  let contador = 1;
-  if (contador === 1) {
-    document.querySelector('nav').animate({
-    left:'0';
-    });
-    contador=0;
-    }else{
-    contador=1;
-    document.querySelector('nav').animate({
-    left:'-100%';
-    });
-    }
-    // console.log('hola chicas')
-    });
