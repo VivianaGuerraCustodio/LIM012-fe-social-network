46e4089e5fc5ff8c0ba728d6b7df005b79a15d46
viviana2397 <vivianaguerracustodio@gmail.com>
2020-04-21 18:27:43 -0500

    firebase register add

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-import { example } from './example.js';
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
 
-example();
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyAwXhQApvJ9tq-KWDkobxKX3eX02aJnTnY',
-  authDomain: 'yachaywasiper.firebaseapp.com',
-  databaseURL: 'https://yachaywasiper.firebaseio.com',
-  projectId: 'yachaywasiper',
-  storageBucket: 'yachaywasiper.appspot.com',
-  messagingSenderId: '310386263852',
-  appId: '1:310386263852:web:73a2899abadb931f214e89',
-  measurementId: 'G-QTBC8WWL0Y',
-};
-const provider = new firebase.auth.FacebookAuthProvider();
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+/* import { example } from './example.js';
+
+example(); */
+const signIn = document.querySelector('.logIn');
+// const login = document.querySelector('.ingresar');
+const buttonRegister = document.querySelector('.register');
+const registerNewUser = document.querySelector('.newUser');
+const registrar = document.querySelector('.nuevoUsuario');
+
+buttonRegister.addEventListener('click', () => {
+  signIn.style.display = 'none';
+  registerNewUser.style.display = 'block';
+});
+
+registrar.addEventListener('click', () => {
+  const email = document.querySelector('.newEmail').value;
+  const password = document.querySelector('.newPassword').value;
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+  });
+});
