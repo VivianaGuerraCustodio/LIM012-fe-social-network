a03afb2af19df808108566d04f3228eb962280f3
viviana2397 <vivianaguerracustodio@gmail.com>
2020-04-22 12:34:56 -0500

    firebase register complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 /* eslint-disable no-console */
 /* eslint-disable no-alert */
-
-
 /* import { example } from './example.js';
 
 example(); */
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAwXhQApvJ9tq-KWDkobxKX3eX02aJnTnY',
+  authDomain: 'yachaywasiper.firebaseapp.com',
+  databaseURL: 'https://yachaywasiper.firebaseio.com',
+  projectId: 'yachaywasiper',
+  storageBucket: 'yachaywasiper.appspot.com',
+  messagingSenderId: '310386263852',
+  appId: '1:310386263852:web:73a2899abadb931f214e89',
+  measurementId: 'G-QTBC8WWL0Y',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 const signIn = document.querySelector('.logIn');
 // const login = document.querySelector('.ingresar');
 const buttonRegister = document.querySelector('.register');
@@ -16,14 +28,20 @@ buttonRegister.addEventListener('click', () => {
   registerNewUser.style.display = 'block';
 });
 
-registrar.addEventListener('click', () => {
+registrar.addEventListener('click', (event) => {
+  event.preventDefault();
   const email = document.querySelector('.newEmail').value;
   const password = document.querySelector('.newPassword').value;
   firebase.auth().createUserWithEmailAndPassword(email, password).catch((error) => {
     // Handle Errors here.
     const errorCode = error.code;
     const errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
+    if (errorCode === 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    signIn.style.display = 'block';
+    registerNewUser.style.display = 'none';
   });
 });
