d51a4f76fa7cddc9bf4e7b4888fe3619f6a6df14
Lourdes0896 <sam.lourdesmerino@gmail.com>
2020-04-27 22:53:22 -0500

    prototipos

--- src/view/login.js
+++ src/view/login.js
@@ -3,19 +3,19 @@ import { logIn } from '../controller/firebase.js';
 export default () => {
   const viewLogin = `
     <form class="logIn"> 
-     <div class="logo"> 
-     <img class="logoPrincipal" src="assets/logo-wasi.png">
-       </div>
-       <p> Bienvenid@s a YachayWasi, una red social para estudiantes, padres y maestros de todo el Perú </p>
-       <div class="formulario">
+    <div class="logo"> 
+    <img class="logoPrincipal" src="assets/logo-wasi.png">
+      </div>
+      <p> Bienvenid@s a YachayWasi, una red social para estudiantes, padres y maestros de todo el Perú </p>
+      <div class="formulario">
         <input class="emailLogin" type="email" placeholder="Correo Electrónico" required> 
         <input class="passwordLogin" type="password" placeholder="Contraseña" required> 
         <button class= "ingresar"> Ingresar </button>
-       </div>
-       <p class="otherOptions">O puedes ingresar con ...</p>
-       <img class="iconFb" src="assets/iconFb.jpg"/>
-       <img class="iconGoogle" src="assets/iconG.png"/>
-       <p>¿No tienes cuenta? , <a class="register"  href="#/register">Regístrate</a> </p>
+      </div>
+      <p class="otherOptions">O puedes ingresar con ...</p>
+      <img class="iconFb" src="assets/iconFb.jpg"/>
+      <img class="iconGoogle" src="assets/iconG.png"/>
+      <p>¿No tienes cuenta? , <a class="register"  href="#/register">Regístrate</a> </p>
     </form>`;
   const sectionElem = document.createElement('section');
   sectionElem.className = 'login';
