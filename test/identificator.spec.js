4c2fb1ec3b579f64bcfd2cb3340b9d89543ac36b
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-04-29 09:26:51 -0500

    iniciando la implementación de test

---
+++ test/identificator.spec.js
@@ -0,0 +1,50 @@
+import {
+  register,
+  logIn,
+  signInOff,
+  googleAuth,
+  facebookAuth,
+} from '../src/controler/firebase.js';
+
+/* falta configuración firebase */
+
+describe('Inicio de sesion', () => {
+  it('Debe iniciar sesión', () => {
+    logIn('usuario@gmail.com', '123456').then((user) => {
+      expect().toBe('usuario@gmail.com');
+      expect().toBe(false);
+    });
+  });
+});
+
+describe('Registro', () => {
+  it('Debe crear usuario nuevo', () => {
+    register('ejemplo@labo.com', '123456').then((user) => {
+      expect().toBe('ejemplo@labo.com');
+      expect().toBe(false);
+    });
+  });
+});
+
+describe('Cierre de sesion', () => {
+  it('Debe cerrar sesión', () => signInOff()
+    .then((user) => {
+      expect(user).toBe(undefined);
+    }));
+});
+
+describe('Inicio-Google', () => {
+  it('Debe iniciar sesión Google', () => googleAuth()
+    .then((user) => {
+      expect().toBe(false);
+      expect().toEqual([{ 'google.com' }]);
+    }));
+});
+
+describe('Inicio-Facebook', () => {
+  it('Debe iniciar sesión Facebook', () => facebookAuth()
+    .then((user) => {
+      expect().toBe(false);
+      expect().toEqual([{  'facebook.com' }]);
+    }));
+});
