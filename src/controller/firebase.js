be0dd3da88b28e5bd8c8a519b48003b2c3dbf97a
viviana2397 <vivianaguerracustodio@gmail.com>
2020-05-06 11:34:25 -0500

    firebase.js

--- src/controller/firebase.js
+++ src/controller/firebase.js
@@ -1,8 +1,23 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable max-len */
 /* eslint-disable import/no-cycle */
 
 
 import { changeView } from '../view-controler/router.js';
+
+const user = () => {
+  firebase.auth().currentUser;
+  let name; let email; let photoUrl; let uid; let
+    emailVerified;
+  if (user != null) {
+    name = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    emailVerified = user.emailVerified;
+    uid = user.uid;
+  }
+};
+// import profile from '../view/profile.js';
 
 export const register = (email, password) => firebase.auth().createUserWithEmailAndPassword(email, password);
 
@@ -13,11 +28,21 @@ export const logIn = (emailLogin, passwordLogin) => firebase.auth().signInWithEm
     }
   });
 
-export const signInOff = () => firebase.auth().signOut().then().catch();
+export const signInOff = () => firebase.auth().signOut().then(() => {
+  changeView('#/login');
+}).catch();
 
 export const googleAuth = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider).then(() => {
+    const db = firebase.firestore();
+    db.collection('usuarios').add({
+      name: user.displayName,
+      email: user.email,
+      photoUrl: user.photoURL,
+      // emailVerified : user.emailVerified,
+      uid: user.uid,
+    });
     if (googleAuth) {
       changeView('#/home');
     }
