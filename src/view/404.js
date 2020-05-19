9f610638d0616457193e229a0aa861caea9e1626
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-05-19 18:05:24 -0500

    history api para error 404

--- src/view/404.js
+++ src/view/404.js
@@ -3,10 +3,16 @@ export default () => {
           <h2>404</h2>
           <h1>Página no encontrada </h1>
           <p> El archivo especificado no se encontró en este sitio web. Por favor compruebe el URL para errores y 
-          vuelva a intentarlo</p> `;
-
+          vuelva a intentarlo.</p> 
+          <button class="back" > Regresar atras </button>`;
   const divElem = document.createElement('div');
   divElem.setAttribute('id', 'message');
   divElem.innerHTML = viewError;
+
+  const back = divElem.querySelector('.back');
+  back.addEventListener('click', () => {
+    window.history.back();
+  });
+
   return divElem;
 };
