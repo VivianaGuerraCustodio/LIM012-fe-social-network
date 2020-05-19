b1b0ca2b33a5a53e4ca15178768f87cc3378375e
viviana2397 <vivianaguerracustodio@gmail.com>
2020-05-19 12:47:56 -0500

    error register

--- src/controller/firebase.js
+++ src/controller/firebase.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable import/no-cycle */
 
 
@@ -9,9 +10,14 @@ import { modelProfile } from '../templates/templateProfile.js';
 
 
 // eslint-disable-next-line max-len
-export const register = (email, password) => firebase.auth().createUserWithEmailAndPassword(email, password);
+export const currentUser = () => firebase.auth().currentUser;
 
-// eslint-disable-next-line max-len
+export const register = (email, password) => firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+  if (register) {
+    saveUser((currentUser(email)));
+  }
+});
+
 export const logIn = (emailLogin, passwordLogin) => firebase.auth().signInWithEmailAndPassword(emailLogin, passwordLogin)
   .then(() => {
     if (logIn) {
@@ -19,7 +25,6 @@ export const logIn = (emailLogin, passwordLogin) => firebase.auth().signInWithEm
     }
   });
 
-export const currentUser = () => firebase.auth().currentUser;
 
 export const signInOff = () => firebase.auth().signOut().then().catch();
 
